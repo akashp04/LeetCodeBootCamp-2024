5c746dee7b2663ef8e6ade3a1488fee6fcb1fc59
Akash <54092294+akashp04@users.noreply.github.com>
2024-03-03 18:05:44 -0500

    Update ContainsDuplicate.py

--- Week-1/ContainsDuplicate.py
+++ Week-1/ContainsDuplicate.py
@@ -7,3 +7,11 @@ class Solution:
             elif count_nums[i] == 1:
                 return True
         return False
+
+# Bit manipulation solution
+# class Solution:
+#     def containsDuplicate(self, nums: List[int]) -> bool:
+#         nums.sort()
+#         for i in range(1, len(nums)):
+#             if nums[i - 1] ^ nums[i] == 0: return True
+#         return False
